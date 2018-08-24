873f716b47f75906ba5f8dadb55f5d3a69907749
Christy Welsh <christyannwelsh@gmail.com>
2018-08-23 17:57:05 -0700

    all tasks complete

--- server/server.js
+++ server/server.js
@@ -1,18 +1,17 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
+let app = express();
 
 // write form submissions in this file
 let filePath = path.join(__dirname, './formsubmissions.json');
 
-let app = express();
-
-// responde with simple text on root
+// respond with simple text on root
 // app.get('/', (req, res) => {                
 //     res.send('Hello from the web server side ...');           
 // })
-
 
 // log url of each root request
 app.use((req, res, next) => {
@@ -23,17 +22,15 @@ app.use((req, res, next) => {
 // use body-parser for form
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// write form submissions to json file when posted; responde with simple text
-app.post('/contact-form', (req, res, next) => {
+// write form submissions to json file when posted; respond with simple text
+app.post('/contact-form', (req, res) => {
     fs.appendFileSync(filePath, `{"name": "${req.body.name}", "email": "${req.body.email}"}\n`);
     res.send("Registration Complete");
-    next();
 })
 
 // send contents of form submissions json file
-app.get('/formsubmissions', (req, res, next) => {
+app.get('/formsubmissions', (req, res) => {
     res.sendFile(filePath, err => console.log(err))
-    next();
 })
 
 // serve up html & css files
